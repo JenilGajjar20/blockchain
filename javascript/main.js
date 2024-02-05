8ee0ab1e1ece26fedc2d217b0a75d133d3316002
Jenil Gajjar <jenil20gajjar@gmail.com>
2024-02-05 15:06:15 +0530

    set difficulty level

--- javascript/main.js
+++ javascript/main.js
@@ -2,49 +2,73 @@ const SHA256 = require("crypto-js/sha256");
 const readline = require("readline");
 
 class Block {
-  constructor(index, timestamp, data, previous_hash) {
+  constructor(index, timestamp, data, previous_hash, difficulty) {
     this.index = index;
     this.timestamp = timestamp;
     this.data = data;
     this.previous_hash = previous_hash;
+    this.difficulty = difficulty;
+    this.nonce = 0;
     this.hash = this.computeHash();
   }
 
   computeHash() {
     // Concatenating block information and calculating SHA-256 hash
     return SHA256(
-      this.index + this.timestamp,
-      this.previous_hash,
-      JSON.stringify(this.data)
-    );
+      this.index +
+        this.timestamp +
+        this.previous_hash +
+        JSON.stringify(this.data) +
+        this.nonce +
+        this.difficulty
+    ).toString();
+  }
+
+  mineNewBlock() {
+    while (
+      this.hash.substring(0, this.difficulty) !==
+      Array(this.difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.computeHash();
+    }
+    console.log(`Block mined: ${this.hash}`);
   }
 }
 
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2; // Set the initial difficulty level
   }
 
   createGenesisBlock() {
     // Creating the first block (genesis block)
-    return new Block(0, new Date().toISOString(), "Genesis Block", "0");
+    return new Block(0, new Date().getFullYear(), "Genesis Block", "0");
   }
 
   addBlock(data) {
+    const newDate = new Date();
     // Adding a new block to the chain
     const index = this.chain.length;
     const previousHash = this.chain[index - 1].hash;
-    const timestamp = new Date().toISOString();
-    const newBlock = new Block(index, timestamp, data, previousHash);
+    const timestamp = new Date().getFullYear();
+    const newBlock = new Block(
+      index,
+      timestamp,
+      data,
+      previousHash,
+      this.difficulty
+    );
+    newBlock.mineNewBlock();
     this.chain.push(newBlock);
   }
 
   printBlockchain() {
     // Printing the information about each block in the blockchain
     this.chain.forEach((block) => {
-      console.log(`Index: ${block.index}`);
       console.log(`Timestamp: ${block.timestamp}`);
-      console.log(`Data: ${block.data}`);
+      // console.log(`Data: ${block.data}`);
       console.log(`Previous Hash: ${block.previous_hash}`);
       console.log(`Hash: ${block.hash}\n`);
     });
