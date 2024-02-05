b2dad7e49f5b57a037ea94d3da7da620e91fe9fb
Jenil Gajjar <jenil20gajjar@gmail.com>
2024-02-05 15:41:49 +0530

    reward distribution

--- javascript/main.js
+++ javascript/main.js
@@ -2,13 +2,23 @@ const SHA256 = require("crypto-js/sha256");
 const readline = require("readline");
 
 class Block {
-  constructor(index, timestamp, data, previous_hash, difficulty) {
+  constructor(
+    index,
+    timestamp,
+    data,
+    previous_hash,
+    difficulty,
+    miner,
+    reward
+  ) {
     this.index = index;
     this.timestamp = timestamp;
     this.data = data;
     this.previous_hash = previous_hash;
     this.difficulty = difficulty;
     this.nonce = 0;
+    this.miner = miner;
+    this.reward = reward;
     this.hash = this.computeHash();
   }
 
@@ -20,7 +30,9 @@ class Block {
         this.previous_hash +
         JSON.stringify(this.data) +
         this.nonce +
-        this.difficulty
+        this.difficulty +
+        this.miner +
+        this.reward
     ).toString();
   }
 
@@ -40,15 +52,23 @@ class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
     this.difficulty = 2; // Set the initial difficulty level
+    this.minerReward = 10; // Set the initial reward for the miner
   }
 
   createGenesisBlock() {
     // Creating the first block (genesis block)
-    return new Block(0, new Date().getFullYear(), "Genesis Block", "0");
+    return new Block(
+      0,
+      new Date().getFullYear(),
+      "Genesis Block",
+      "0",
+      this.difficulty,
+      "Genesis Miner",
+      0
+    );
   }
 
-  addBlock(data) {
-    const newDate = new Date();
+  addBlock(data, miner) {
     // Adding a new block to the chain
     const index = this.chain.length;
     const previousHash = this.chain[index - 1].hash;
@@ -58,7 +78,9 @@ class Blockchain {
       timestamp,
       data,
       previousHash,
-      this.difficulty
+      this.difficulty,
+      miner,
+      this.minerReward
     );
     newBlock.mineNewBlock();
     this.chain.push(newBlock);
@@ -68,6 +90,8 @@ class Blockchain {
     // Printing the information about each block in the blockchain
     this.chain.forEach((block) => {
       console.log(`Timestamp: ${block.timestamp}`);
+      // console.log(`Miner: ${block.miner}`);
+      console.log(`Reward: ${block.reward}`);
       // console.log(`Data: ${block.data}`);
       console.log(`Previous Hash: ${block.previous_hash}`);
       console.log(`Hash: ${block.hash}\n`);
@@ -87,8 +111,10 @@ r1.question("Enter the number of blocks in the blockchain: ", (numBlocks) => {
   const addBlocks = (i) => {
     if (i < numBlocks) {
       r1.question(`Enter data for block ${i + 1}: `, (data) => {
-        blockchain.addBlock(data);
-        addBlocks(i + 1);
+        r1.question(`Enter miner for block ${i + 1}: `, (miner) => {
+          blockchain.addBlock(data, miner);
+          addBlocks(i + 1);
+        });
       });
     } else {
       r1.close();
